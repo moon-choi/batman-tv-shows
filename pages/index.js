fab08ce3822e640393b160efef6d4d14669b41d4
Moon Choi <choimy1991@gmail.com>
2022-09-05 17:31:55 -0700

    Next js 튜토리얼 4편 : 동적 페이지

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
+
 
 /* method 1
 import withLayout from '../components/Layout'
@@ -6,11 +8,33 @@ const Page = () => <p>Hello Next.js</p>
 export default withLayout(Page)
 */
 
-// method 0
+/* method 0
 export default function Index() {
   return (
     <Layout>
       <p>this is index.js page</p>
+    </Layout>
+  )
+}
+*/
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" /> 
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   )
 }
